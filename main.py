5711976c25e26e63f2bbe2fc8c4239b5519b190f
Vladislav Mashin <mashinvladislav@gmail.com>
2022-12-06 15:02:54 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import datetime
+import time
+
+dt = datetime.datetime.today().strftime("%d-%m-%Y")
+tc = time.strftime("%H.%M.%S")
+
+print("Local Date: ", tc + " " + dt, "\n")
+
+import salary
+
+print("Module salary was imported", "\n")
+
+import people
+
+print("Module people was imported", "\n")
+
+if __name__ == '__main__':
+    salary.calculate_salary()
+    people.get_employees()
+print("Закончено выполнение if __name__ == '__main__'")
